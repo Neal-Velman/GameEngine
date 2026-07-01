52e7878aada6e93b0c7a7a5bae4335532a1ac607
nvelman <nvelman@NVELMAN>
2026-07-01 09:51:29 -0600

    Add project files.

---
+++ Engine/Engine.cpp
@@ -0,0 +1,13 @@
+// Engine.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include <iostream>
+#include "engine.h"
+// TODO: This is an example of a library function
+void fnEngine()
+{
+	std::cout << "Welcome to the library.\n";
+}
+
